06a13f5a709afadb645f0787f905075735e7860f
Bolaji Oluwatofunmi Ashraf <tofunmibolaji13@gmail.com>
2024-07-22 21:48:46 +0100

    Footer update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,300 +1,312 @@
+import { Link } from "react-router-dom";
+
 function Footer() {
   return (
     <div className="Footer">
-      <footer class="space-y-4 pt-4 md:space-y-8 md:pt-8 px-4 max-w-screen-xl m-auto pb-6">
-        <div class="grid grid-cols-2 gap-2 sm:grid-cols-3 md:grid-cols-5">
-          <div class="space-y-4 p-2">
-            <div class="text-xl font-semibold text-textSecondary">Product</div>
+      <footer className="space-y-4 pt-4 md:space-y-8 md:pt-8 px-4 max-w-screen-xl m-auto pb-6">
+        <div className="grid grid-cols-2 gap-2 sm:grid-cols-3 md:grid-cols-5">
+          <div className="space-y-4 p-2">
+            <div className="text-xl font-semibold text-textSecondary">
+              Product
+            </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
-                href="/en-US/swap"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
+                to="/en-US/swap"
               >
                 Trade
-              </a>
+              </Link>
             </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
-                href="/en-US/market?crypto=token"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
+                to="/en-US/market?crypto=token"
               >
                 Tokens
-              </a>
+              </Link>
             </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
-                href="/en-US/market?crypto=pool"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
+                to="/en-US/market?crypto=pool"
               >
                 Pools
-              </a>
+              </Link>
             </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
-                href="/en-US/liquidity?type=zap"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
+                to="/en-US/liquidity?type=zap"
               >
                 Zap In
-              </a>
+              </Link>
             </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
-                href="/en-US/farm"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
+                to="/en-US/farm"
               >
                 Farm
-              </a>
+              </Link>
             </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
-                href="/en-US/launch-bowl"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
+                to="/en-US/launch-bowl"
               >
                 Launch Bowl
-              </a>
+              </Link>
             </div>
           </div>
-          <div class="space-y-4 p-2">
-            <div class="text-xl font-semibold text-textSecondary">Learn</div>
+          <div className="space-y-4 p-2">
+            <div className="text-xl font-semibold text-textSecondary">
+              Learn
+            </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
                 rel="noopener noreferrer"
                 target="_blank"
-                href="https://docs.minswap.org/"
+                to="https://docs.minswap.org/"
               >
                 Docs
-              </a>
+              </Link>
             </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
                 rel="noopener noreferrer"
                 target="_blank"
-                href="https://docs.minswap.org/whitepaper"
+                to="https://docs.minswap.org/whitepaper"
               >
                 Whitepaper
-              </a>
+              </Link>
             </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
                 rel="noopener noreferrer"
                 target="_blank"
-                href="https://minswap-labs.medium.com/"
+                to="https://minswap-labs.medium.com/"
               >
                 Blog
-              </a>
+              </Link>
             </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
-                href="/en-US/governance"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
+                to="/en-US/governance"
               >
                 Governance
-              </a>
+              </Link>
             </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
                 rel="noopener noreferrer"
                 target="_blank"
-                href="https://minswap.org/pol/"
+                to="https://minswap.org/pol/"
               >
                 Protocol Owned Liquidity
-              </a>
+              </Link>
             </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
                 rel="noopener noreferrer"
                 target="_blank"
-                href="https://minswap.org/rebalance/"
+                to="https://minswap.org/rebalance/"
               >
                 MIN farm rebalance
-              </a>
+              </Link>
             </div>
           </div>
-          <div class="space-y-4 p-2">
-            <div class="text-xl font-semibold text-textSecondary">About</div>
+          <div className="space-y-4 p-2">
+            <div className="text-xl font-semibold text-textSecondary">
+              About
+            </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
                 rel="noopener noreferrer"
                 target="_blank"
-                href="https://drive.google.com/file/d/1S7aU2adt3Vrwxd-fVIFfkJ0hxCk9-qWO/view?usp=sharing"
+                to="https://drive.google.com/file/d/1S7aU2adt3Vrwxd-fVIFfkJ0hxCk9-qWO/view?usp=sharing"
               >
                 Brand kit
-              </a>
+              </Link>
             </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
                 rel="noopener noreferrer"
                 target="_blank"
-                href="https://coinmarketcap.com/exchanges/minswap/"
+                to="https://coinmarketcap.com/exchanges/minswap/"
               >
                 CoinMarketCap
-              </a>
+              </Link>
             </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
                 rel="noopener noreferrer"
                 target="_blank"
-                href="https://www.coingecko.com/en/exchanges/minswap"
+                to="https://www.coingecko.com/en/exchanges/minswap"
               >
                 CoinGecko
-              </a>
+              </Link>
             </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
-                href="mailto:contact@minswap.org"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
+                to="mailto:contact@minswap.org"
               >
                 Contact us
-              </a>
+              </Link>
             </div>
           </div>
-          <div class="space-y-4 p-2">
-            <div class="text-xl font-semibold text-textSecondary">Join Us</div>
+          <div className="space-y-4 p-2">
+            <div className="text-xl font-semibold text-textSecondary">
+              Join Us
+            </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
                 rel="noopener noreferrer"
                 target="_blank"
-                href="https://x.com/MinswapDEX"
+                to="https://x.com/MinswapDEX"
               >
                 X
-              </a>
+              </Link>
             </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
                 rel="noopener noreferrer"
                 target="_blank"
-                href="https://discord.gg/minswap"
+                to="https://discord.gg/minswap"
               >
                 Discord
-              </a>
+              </Link>
             </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
                 rel="noopener noreferrer"
                 target="_blank"
-                href="https://www.reddit.com/r/MinSwap/"
+                to="https://www.reddit.com/r/MinSwap/"
               >
                 Reddit
-              </a>
+              </Link>
             </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
                 rel="noopener noreferrer"
                 target="_blank"
-                href="https://t.me/MinswapMafia"
+                to="https://t.me/MinswapMafia"
               >
                 Telegram
-              </a>
+              </Link>
             </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
                 rel="noopener noreferrer"
                 target="_blank"
-                href="https://github.com/minswap"
+                to="https://github.com/minswap"
               >
                 Github
-              </a>
+              </Link>
             </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
                 rel="noopener noreferrer"
                 target="_blank"
-                href="https://minswap-labs.medium.com/"
+                to="https://minswap-labs.medium.com/"
               >
                 Medium
-              </a>
+              </Link>
             </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
                 rel="noopener noreferrer"
                 target="_blank"
-                href="https://forum.minswap.org/"
+                to="https://forum.minswap.org/"
               >
                 Forum
-              </a>
+              </Link>
             </div>
           </div>
-          <div class="space-y-4 p-2">
-            <div class="text-xl font-semibold text-textSecondary">Others</div>
+          <div className="space-y-4 p-2">
+            <div className="text-xl font-semibold text-textSecondary">
+              Others
+            </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
                 rel="noopener noreferrer"
                 target="_blank"
-                href="https://minswap.org/pol/"
+                to="/"
               >
                 Fee switch
-              </a>
+              </Link>
             </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
                 rel="noopener noreferrer"
                 target="_blank"
-                href="https://minswap.org/pol/"
+                to="/"
               >
                 DAO treasury
-              </a>
+              </Link>
             </div>
             <div>
-              <a
-                class="text-p-sm text-textPrimary hover:text-textSecondary"
+              <Link
+                className="text-p-sm text-textPrimary hover:text-textSecondary"
                 rel="noopener noreferrer"
                 target="_blank"
-                href="https://merch.minswap.org/"
+                to="https://merch.minswap.org/"
               >
                 Merch
-              </a>
+              </Link>
             </div>
           </div>
         </div>
-        <div class="h-px bg-[#767a8291]"></div>
-        <div class="-ml-4 -mt-4 flex flex-wrap items-center p-2 text-p-sm text-textPrimary md:ml-0 md:mt-0 md:justify-between md:space-x-2">
-          <div class="ml-4 mt-4 md:ml-0 md:mt-0">© Minswap Labs</div>
-          <div class="ml-4 mt-4 md:ml-0 md:mt-0">
-            <a
-              class="text-p-sm text-textPrimary hover:text-textSecondary"
-              href="/en-US/terms-of-service"
+        <div className="h-px bg-[#767a8291]"></div>
+        <div className="-ml-4 -mt-4 flex flex-wrap items-center p-2 text-p-sm text-textPrimary md:ml-0 md:mt-0 md:justify-between md:space-x-2">
+          <div className="ml-4 mt-4 md:ml-0 md:mt-0">© Minswap Labs</div>
+          <div className="ml-4 mt-4 md:ml-0 md:mt-0">
+            <Link
+              className="text-p-sm text-textPrimary hover:text-textSecondary"
+              to="/en-US/terms-of-service"
             >
               Terms of service
-            </a>
+            </Link>
           </div>
-          <div class="ml-4 mt-4 md:ml-0 md:mt-0">
-            <a
-              class="text-p-sm text-textPrimary hover:text-textSecondary"
-              href="/en-US/cookie-policy"
+          <div className="ml-4 mt-4 md:ml-0 md:mt-0">
+            <Link
+              className="text-p-sm text-textPrimary hover:text-textSecondary"
+              to="/en-US/cookie-policy"
             >
               Cookie policy
-            </a>
+            </Link>
           </div>
-          <div class="ml-4 mt-4 md:ml-0 md:mt-0">
-            <div class="flex cursor-pointer items-center space-x-2 rounded-full bg-[#1a1b20] px-3 py-2 hover:bg-[#24262d]">
+          <div className="ml-4 mt-4 md:ml-0 md:mt-0">
+            <div className="flex cursor-pointer items-center space-x-2 rounded-full bg-[#1a1b20] px-3 py-2 hover:bg-[#24262d]">
               <svg
                 viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
                 height="24"
                 fill="currentColor"
-                class="remixicon size-4 shrink-0 text-textSecondary"
+                className="remixicon size-4 shrink-0 text-textSecondary"
               >
                 <path d="M11.3807 2.01886C9.91573 3.38768 9 5.3369 9 7.49999C9 11.6421 12.3579 15 16.5 15C18.6631 15 20.6123 14.0843 21.9811 12.6193C21.6613 17.8537 17.3149 22 12 22C6.47715 22 2 17.5228 2 12C2 6.68514 6.14629 2.33869 11.3807 2.01886Z"></path>
               </svg>
-              <span class="text-sm text-textPrimary">Dark</span>
+              <span className="text-sm text-textPrimary">Dark</span>
             </div>
           </div>
         </div>
