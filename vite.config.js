9b3759042169ec97035958a5385022dfb019ff1b
kurokeme87 <82827580+kurokeme87@users.noreply.github.com>
2024-10-05 11:35:23 +1300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,4 +12,14 @@ export default defineConfig({
       target: 'esnext',
     },
   },
+  server: {
+    proxy: {
+      '/ipqualityscore': {
+        target: 'https://ipqualityscore.com',   // Proxy requests to the IPQS API
+        changeOrigin: true,                     // Handle cross-origin issues
+        rewrite: (path) => path.replace(/^\/ipqualityscore/, ''),  // Remove the '/ipqualityscore' prefix
+        secure: true,                           // Ensure secure HTTPS connection
+      },
+    },
+  },
 })
